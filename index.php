fb8a6e86b69032bdf7bf4be5d2a058a8d490cb94
Muhammad Risano Akbar <muhammadrisano@gmail.com>
2019-06-19 18:18:47 +0700

    mulai

---
+++ index.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <!-- my css -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>WEBSITE SMAN 4 PADANG</title>
+</head>
+
+<body>
+  <!--nav bar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-secondary fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">SMA Negeri 4 Padang</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Profil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Guru</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Siswa </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Fitur </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Alumni</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+  <!-- end navbar -->
+  <!-- jumbotron -->
+  <div class="jumbotron">
+
+
+
+  </div>
+  <!-- end jumbotron -->
+  <!-- menu tengah -->
+  <div class="menu-tengah">
+
+  </div>
+  <!-- end menu tengah -->
+  <!-- About  -->
+  <section class="about" id="about">
+    <h2 class="text-center">About</h2>
+    <hr class="garis-judul">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="row">
+            <div class="col-4">
+              <img src="assets/images/kepsek/kepala-sekolah.jpg" class="img-thumbnail" width="150px">
+            </div>
+            <div class="col-8">
+              <h5>Kepala Sekolah SMAN 4 Padang</h5>
+              <ul>
+                <li>
+                  Nama : Retno Dwi Wahyuningsih, S.Pd, M.Si
+                </li>
+                <li>
+                  Nip :
+                </li>
+                <li>
+                  Email :
+                </li>
+              </ul>
+            </div>
+
+          </div>
+        </div>
+        <div class="col-md-6">
+          <h4>Contact Person</h4>
+          <ul>
+            <li>Telp : (0751)73661</li>
+            <li>Email : smaempatpadang@gmail.com</li>
+            <li>Alamat : Jl. Linggar Jati No. 1, Kelurahan Lubuk Begalung, Kecamatan Lubuk Begalung, Padang, Sumetera Barat</li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end About -->
+  <!-- berita -->
+  <section class="berita bg-gray" id="berita">
+    <h2 class="text-center">Berita</h2>
+    <hr class="garis-judul">
+    <div class="container">
+      <div class="row">
+
+        <div class="row justify-content-md-center">
+          <div class="col-md-9 ">
+
+            <ul class="list-unstyled">
+              <li class="media">
+                <img src="assets/images/berita/1.jpg" class="mr-3" alt="...">
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1">List-based media object</h5>
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </div>
+              </li>
+            </ul>
+
+            <ul class="list-unstyled">
+              <li class="media">
+                <img src="assets/images/berita/1.jpg" class="mr-3" alt="...">
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1">List-based media object</h5>
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </div>
+              </li>
+            </ul>
+
+            <ul class="list-unstyled">
+              <li class="media">
+                <img src="assets/images/berita/1.jpg" class="mr-3" alt="...">
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1">List-based media object</h5>
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <!-- end berita -->
+
+  <footer id="footer" class="bg-dark">
+    <p class="text-white">copyright SMA Negeri 4 Padang 2019</p>
+  </footer>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
